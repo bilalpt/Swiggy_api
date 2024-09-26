0a746a2aee90e802dbcd951958b8843aafc750ce
bilalpt <bilalpt572@gmil.com>
2024-09-26 07:11:38 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import index from '../src/index.css';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='logo-container'>
+        <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All' />
+      </div>
+      <div className='nav-items'>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
